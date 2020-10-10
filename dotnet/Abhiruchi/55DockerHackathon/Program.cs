aee39d59a253ae0cdffdce2cc741ca33d25a3506
AbhiRuchiK <abhiruchik64@gmail.com>
2020-10-10 16:11:57 +0530

    Create dotnet console app

---
+++ dotnet/Abhiruchi/55DockerHackathon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _55DockerHackathon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
